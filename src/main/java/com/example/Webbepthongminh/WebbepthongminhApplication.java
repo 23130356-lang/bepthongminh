752b4c5498fee34069356bc898ece0aa213526aa
THANH TRUONG <23130356@st.hcmuaf.edu.vn>
2025-12-10 21:03:30 +0700

    Initial commit

---
+++ src/main/java/com/example/Webbepthongminh/WebbepthongminhApplication.java
@@ -0,0 +1,13 @@
+package com.example.Webbepthongminh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbepthongminhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbepthongminhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Webbepthongminh/WebbepthongminhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Webbepthongminh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbepthongminhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
